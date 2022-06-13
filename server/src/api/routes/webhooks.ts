f7ca590aa09a34420accc7569235b39b3888e685
Dan Harsanyi <dan@danharsanyi.com>
2022-06-13 17:55:06 +1000

    Add slack notification support

--- server/src/api/routes/webhooks.ts
+++ server/src/api/routes/webhooks.ts
@@ -3,8 +3,11 @@ import { Router } from 'express';
 import config from '../../config/index';
 import drip from '../../modules/drip/index';
 import { Eth } from '../../modules/eth/eth';
+import { Slack } from '../../modules/slack/slack';
 
 const router = Router();
+
+const slack = Slack({url: config.dripSlackNotifyUrl});
 
 export default (app: Router) => {
   app.use( '/v1/webhooks', router );
@@ -15,18 +18,39 @@ export default (app: Router) => {
 
   // Process drip claim/hydrate
   router.post( '/drip', async ( req, res ) => {
-    try {
-      const {wallet, key, action} = req.body;
+    const {wallet, key, action} = req.body;
 
+    try {
       if (!['hydrate', 'claim'].includes(action)) {
         return res.send("Supported actions are 'hydrate' and 'claim'");
       }
       
       const eth = Eth({wallet, key, chain: config.bscChain});
       const result = await drip.run(eth, {action});
+
+      const message = slack.formatMessage({
+        method: action,
+        success: result.status,
+        gasUsed: result.gasUsed,
+        transactionHash: result.transactionHash,
+        wallet,
+      });
+
+      await slack.post(message);
+      
       return res.send( result );
     } catch( error ) {
       console.error('error', error);
+
+      const message = slack.formatMessage({
+        method: `${action}: ${error}`,
+        success: false,
+        gasUsed: 0,
+        wallet,
+      });
+
+      await slack.post(message);
+
       return res.send({ error });
     }
   });
--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -4,5 +4,6 @@ dotenv.config();
 export default {
   port: process.env.PORT || 8080,
   dripContractAddress: process.env.DRIP_CONTRACT_ADDRESS,
+  dripSlackNotifyUrl: process.env.DRIP_SLACK_NOTIFY_URL,
   bscChain: process.env.BSC_CHAIN,
 }
